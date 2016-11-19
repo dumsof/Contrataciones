02206b923588e0a5da2a08dcfd3dcf6cf9f36cc2
dumsof <darwinu34@yahoo.es>
2016-11-19 09:06:14 -0500

    se finaliza el menu dinamico

--- Contrataciones/Scripts/ScriptsUsuario/jsPartialMenu.js
+++ Contrataciones/Scripts/ScriptsUsuario/jsPartialMenu.js
@@ -1,13 +1,4 @@
-﻿$(function () {
-
-    ValidarUsuarioLogueado();
-    var swUsuarioLogueado =sessionStorage.getItem('swUsuarioLogueado');
-    if (swUsuarioLogueado == "0") {
-        //$("#menuPrincipal").append('');
-        $("#menuPrincipal").empty();
-        //location.reload();
-        return;
-    }
+﻿$(function () {    
     var menu = sessionStorage.getItem('menu');
     if (menu != null && menu.length > 0) {
         $("#menuPrincipal").append(menu);
@@ -17,25 +8,25 @@
     }
 });
 
-function ValidarUsuarioLogueado()
-{
-    var usuarioLogueado = "0";
-    $.ajax({
-        type: "GET",
-        url: "MenuDinamico/UsuarioAutentificado",
-        data: "",
-        contentType: "application/json;charset=utf-8",
-        dataType: "json",
-        success: function (dataUsuarioLogueado) {            
-            sessionStorage.setItem('swUsuarioLogueado', dataUsuarioLogueado);
-        },
-        error: function (xhr, ajaxOptions, thrownError) {
-            console.log(xhr.status);
-            console.log(thrownError);
-        }
-    });
-    return usuarioLogueado;
-}
+//function ValidarUsuarioLogueado()
+//{
+//    var usuarioLogueado = "0";
+//    $.ajax({
+//        type: "GET",
+//        url: "MenuDinamico/UsuarioAutentificado",
+//        data: "",
+//        contentType: "application/json;charset=utf-8",
+//        dataType: "json",
+//        success: function (dataUsuarioLogueado) {            
+//            sessionStorage.setItem('swUsuarioLogueado', dataUsuarioLogueado);
+//        },
+//        error: function (xhr, ajaxOptions, thrownError) {
+//            console.log(xhr.status);
+//            console.log(thrownError);
+//        }
+//    });
+//    return usuarioLogueado;
+//}
 
 function GenerarMenu() {
     $.ajax({
@@ -47,8 +38,7 @@ function GenerarMenu() {
         success: function (dataMenu) {
             if (dataMenu == null)
             {
-                sessionStorage.setItem('menu', '');
-                //location.reload();
+                sessionStorage.setItem('menu', '');              
                 return;
             }
             var n = 0;
@@ -67,7 +57,7 @@ function GenerarMenu() {
             strMenuTodo += "<li><a href=\"" + url + "elmah.axd\" target=\"_blank\">Errores Aplicación</a></li>";
             $("#menuPrincipal").append(strMenuTodo);
             sessionStorage.setItem('menu', strMenuTodo);
-            //location.reload(false);            
+            location.reload(false);            
         },
         error: function (xhr, ajaxOptions, thrownError) {
             console.log(xhr.status);
